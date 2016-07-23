1e91fdec537f4f9259683fb238016760948dba94
Takeo Kunishima <t.kunishi@gmail.com>
2016-07-23 15:25:53 +0900

    use _ for assigned but unused variables. fix indentation.

--- lib/algebra/algebraic-equation.rb
+++ lib/algebra/algebraic-equation.rb
@@ -4,37 +4,35 @@
 #
 # Version 1.11 (2002.02.23)
 
-require "algebra/polynomial"
-require "algebra/polynomial-factor"
-require "algebra/m-polynomial"
-require "algebra/groebner-basis"
-require "algebra/residue-class-ring"
-require "algebra/splitting-field"
+require 'algebra/polynomial'
+require 'algebra/polynomial-factor'
+require 'algebra/m-polynomial'
+require 'algebra/groebner-basis'
+require 'algebra/residue-class-ring'
+require 'algebra/splitting-field'
 
 module Algebra
   module AlgebraicEquation
     def symmetric_product(objs)
       a = [1]
       objs.each do |x|
-	ax = a.collect{|t| t * x}
-	a.each_index do |i|
-	  if i > 0 && i < a.size
-	    a[i] = a[i] + ax[i-1]
-	  end
-	end
-	a.push ax.last
+        ax = a.collect { |t| t * x }
+        a.each_index do |i|
+          a[i] = a[i] + ax[i - 1] if i > 0 && i < a.size
+        end
+        a.push ax.last
       end
       a
     end
 
     def minimal_polynomial(e, *fs)
-      raise "too many polys" if fs.size > "x".ord - "a".ord #("a"..."x").size
-      require "algebra/groebner-basis"
-      (mpring = e.class).vars("x")
-      x = mpring.var("x")
-      gb = mpring.with_ord(:lex) {
-	Algebra::Groebner.basis([x - e, * fs])
-      }
+      raise 'too many polys' if fs.size > 'x'.ord - 'a'.ord # ("a"..."x").size
+      require 'algebra/groebner-basis'
+      (mpring = e.class).vars('x')
+      x = mpring.var('x')
+      gb = mpring.with_ord(:lex) do
+        Algebra::Groebner.basis([x - e, * fs])
+      end
       gb.last
     end
 
@@ -43,16 +41,16 @@ module Algebra
   end
 end
 
-if __FILE__ == $0
+if __FILE__ == $PROGRAM_NAME
   include Algebra::AlgebraicEquation
-  require "algebra/rational"
+  require 'algebra/rational'
   PQ = Algebra.MPolynomial(Rational)
-  a, b, c, d = PQ.vars("abcd")
-  p minimal_polynomial(a + b, a**3-2, b**2+b+1)
-  p minimal_polynomial(a + b + c, a**2-2, b**2-3, c**2-5)
-#  p minimal_polynomial(a + b, a**5-2, b**4+b**3+b**2+b+1)
-#x^20 + 5x^19 + 15x^18 + 35x^17 + 70x^16 + 113x^15 + 155x^14 + 215x^13 + 410x^12 + 1095x^11 + 1929x^10 + 2185x^9 + 1180x^8 - 905x^7 - 835x^6 + 17x^5 + 40x^4 - 255x^3 - 45x^2 + 135x + 81
+  a, b, c, _d = PQ.vars('abcd')
+  p minimal_polynomial(a + b, a**3 - 2, b**2 + b + 1)
+  p minimal_polynomial(a + b + c, a**2 - 2, b**2 - 3, c**2 - 5)
+  #  p minimal_polynomial(a + b, a**5-2, b**4+b**3+b**2+b+1)
+  # x^20 + 5x^19 + 15x^18 + 35x^17 + 70x^16 + 113x^15 + 155x^14 + 215x^13 + 410x^12 + 1095x^11 + 1929x^10 + 2185x^9 + 1180x^8 - 905x^7 - 835x^6 + 17x^5 + 40x^4 - 255x^3 - 45x^2 + 135x + 81
 
-  #too difficult, deg == 16
-  #p minimal_polynomial(a+b+c+d, a**2-2, b**2-3, c**2-5, d**2-7)
+  # too difficult, deg == 16
+  # p minimal_polynomial(a+b+c+d, a**2-2, b**2-3, c**2-5, d**2-7)
 end
