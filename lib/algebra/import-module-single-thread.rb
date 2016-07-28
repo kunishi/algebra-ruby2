521f59d582db40e1f8ed5834ae72ecfe64c37f90
Takeo Kunishima <t.kunishi@gmail.com>
2016-07-28 12:51:43 +0900

    beautify.

--- lib/algebra/import-module-single-thread.rb
+++ lib/algebra/import-module-single-thread.rb
@@ -1,16 +1,16 @@
-#$IMPORT_MODULE_debug = true
-require "algebra/import-module"
+# $IMPORT_MODULE_debug = true
+require 'algebra/import-module'
 module Import_Module
   class Scope
     def set_methods
       meths = @target.get_orig_methods(@source)
       @target.def_orig_methods meths
-      #def_methods
+      # def_methods
       mod = @mod
-      @klass.class_eval do include mod end
+      @klass.class_eval { include mod }
     end
 
-    def push()
+    def push
       def_methods
       c = @target.stack.current
       @target.stack.push(update(c))
@@ -22,10 +22,11 @@ module Import_Module
     end
 
     private
+
     def update(c)
       d = c.dup
       @source.methods.__each__ do |meth|
-	d[meth] = @mod
+        d[meth] = @mod
       end
       d
     end
@@ -34,25 +35,25 @@ module Import_Module
       mod = @mod
       target = @target
       @source.methods.__each__ do |meth|
-	@klass.module_eval do
-	  alias_method meth, Import_Module.name(meth, mod)
-	  protected(meth) if target.protecteds.include?(meth)
-	  private(meth) if target.privates.include?(meth)
-	end
+        @klass.module_eval do
+          alias_method meth, Import_Module.name(meth, mod)
+          protected(meth) if target.protecteds.include?(meth)
+          private(meth) if target.privates.include?(meth)
+        end
       end
     end
 
     def recov_methods(c)
       orig_methods = @target.orig_methods
       d = @target.stack.current
-      c.__each__ do |meth, mod|
-	@klass.module_eval do
-	  if m = d[meth] || orig_methods[meth] && :orig
-	    alias_method meth, Import_Module.name(meth, m)
-	  else
-	    undef_method meth
-	  end
-	end
+      c.__each__ do |meth, _mod|
+        @klass.module_eval do
+          if m = d[meth] || orig_methods[meth] && :orig
+            alias_method meth, Import_Module.name(meth, m)
+          else
+            undef_method meth
+          end
+        end
       end
     end
   end
@@ -61,18 +62,19 @@ module Import_Module
     def initialize(klass)
       @scopes = {}
       @klass = klass
-      @publics = @klass.public_instance_methods(true)#.find_all{|m| @klass.public_method_defined? m}
-      @privates = @klass.private_instance_methods(true)#.find_all{|m| @klass.private_method_defined? m}
-      @protecteds = @klass.protected_instance_methods(true)#.find_all{|m| @klass.protected_method_defined? m}
+      @publics = @klass.public_instance_methods(true) # .find_all{|m| @klass.public_method_defined? m}
+      @privates = @klass.private_instance_methods(true) # .find_all{|m| @klass.private_method_defined? m}
+      @protecteds = @klass.protected_instance_methods(true) # .find_all{|m| @klass.protected_method_defined? m}
       @orig_methods = {}
       @saved_methods = {}
       resist_orig_methods
-      @stack = Stack.new([Hash.new]) # diff
+      @stack = Stack.new([{}]) # diff
     end
   end
 
   class Stack
     private
+
     def export_current; end
   end
 end
--- lib/algebra/m-polynomial-gcd.rb
+++ lib/algebra/m-polynomial-gcd.rb
@@ -1,6 +1,6 @@
-require "algebra/polynomial"
-require "algebra/m-polynomial"
-#require "algebra/polynomial-converter"
+require 'algebra/polynomial'
+require 'algebra/m-polynomial'
+# require "algebra/polynomial-converter"
 
 module Algebra
   class MPolynomial
@@ -12,28 +12,29 @@ module Algebra
       k = h.value_on(self.class)
       k
     end
-    
+
     def gcd_all(*a)
       t = self
       a.each do |x|
-	break if t.unit?
-	t = t.gcd(x)
+        break if t.unit?
+        t = t.gcd(x)
       end
       t
     end
   end
 end
 
-if $0 == __FILE__
-  require "algebra/residue-class-ring"
+if $PROGRAM_NAME == __FILE__
+  require 'algebra/residue-class-ring'
   include Algebra
   Z7 = ResidueClassRing(Integer, 7)
   P = MPolynomial(Z7)
-#  P = MPolynomial(Integer)
-  x, y, z = P.vars("xyz")
-  
-  f, g  = (x + y) * (x + 2*y), (x + 2*y) * (x + 3*y)
-#  f, g  = x**2*y, x*y**2
+  #  P = MPolynomial(Integer)
+  x, y, z = P.vars('xyz')
+
+  f = (x + y) * (x + 2 * y)
+  g = (x + 2 * y) * (x + 3 * y)
+  #  f, g  = x**2*y, x*y**2
   p k = f.gcd(g)
-  p k/3 == x + 2*y
+  p k / 3 == x + 2 * y
 end
